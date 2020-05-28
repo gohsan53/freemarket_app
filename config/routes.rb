135a82b7fa60d00b3c0ac33ee3bd4f967fc486fe
gohsan53 <s.kitagoh@gmail.com>
2020-05-28 14:01:59 +0900

    Add new item scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :users, only: [:edit]
   resources :members, only: [:update]
+  resources :users, only: [:edit, :update]
+  resources :items, only: :new
 end
