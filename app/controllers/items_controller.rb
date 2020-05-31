af1df3a9ceab4566a270157fdb3a1696058be587
gohsan53 <s.kitagoh@gmail.com>
2020-05-31 14:34:44 +0900

    Create image table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :condition, :explanation, images_attributes: [:src]).merge(user_id: current_user.id)
   end
 end
